161dd3c06b142489700180f592d1e039162043d0
Johannes Heck <johannes@railslove.com>
2014-09-04 14:09:15 +0200

    fixed spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
 require 'pry-byebug'
-require 'contactually'
+require 'contactually-api'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
